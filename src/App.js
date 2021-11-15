4695f5c8d26d36c36321188b87248bb65a94faa0
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-11-16 02:17:15 +0500

    Add comp in app

--- src/App.js
+++ src/App.js
@@ -6,25 +6,32 @@ import {
   NavLink,
 } from 'react-router-dom';
 
+// Components
+import Rockets from './components/Rockets';
+import Missions from './components/Missions';
+
 function App() {
   return (
     <Router>
       <div>
         <nav>
+          <img src="" alt="logo" />
           <ul>
             <li>
-              {' '}
               <NavLink to="/">Rockets</NavLink>
             </li>
             <li>
-              {' '}
               <NavLink to="/missions">Missions</NavLink>
             </li>
           </ul>
         </nav>
         <Switch>
-          <Route exact path="/" />
-          <Route exact path="/missions" />
+          <Route exact path="/">
+            <Rockets />
+          </Route>
+          <Route exact path="/missions">
+            <Missions />
+          </Route>
         </Switch>
       </div>
     </Router>

0af6d00238a6231d04f03a0ce08f3709c5f0b08a
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-11-16 02:23:20 +0500

    Add logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import {
   BrowserRouter as Router,
   Switch,
@@ -7,15 +6,17 @@ import {
 } from 'react-router-dom';
 
 // Components
+import './App.css';
 import Rockets from './components/Rockets';
 import Missions from './components/Missions';
+import logo from './assets/img/logo.png';
 
 function App() {
   return (
     <Router>
       <div>
         <nav>
-          <img src="" alt="logo" />
+          <img src={logo} alt="logo" width="100px" height="100px" />
           <ul>
             <li>
               <NavLink to="/">Rockets</NavLink>

f02594a011b144333723bf428c9cd19a93d2b2db
shadabwahidullah <shadabwahidullah@gmail.com>
2021-11-16 00:33:27 +0430

    Install and Setup Redux logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,7 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, applyMiddleware } from "@reduxjs/toolkit";
+import logger from "redux-logger";
 
 export default configureStore ({
     reducer: {},
+    applyMiddleware(logger)
 })

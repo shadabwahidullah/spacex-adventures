9bd8947ce9b326605206a210928ef9119ca9ed29
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-11-17 01:57:23 +0500

    Create missions style file

--- src/App.js
+++ src/App.js
@@ -8,39 +8,50 @@ import {
 // Components
 import './App.css';
 import Rockets from './components/Rockets';
-import Missions from './components/Missions';
+import Missions from './components/missions/Missions';
 import logo from './assets/img/logo.png';
 import MyProfile from './components/MyProfile';
 
 function App() {
+  // if (location.pathname === '/missions') {
+  // // }
+  // className={(isActive) =>
+  //   'nav-link' + (!isActive ? ' ' : 'active')
+  // // }
   return (
     <Router>
       <div className="container">
         <nav className="navbar">
-          <img className="logo" src={logo} alt="logo" width="100px" height="100px" />
+          <img
+            className="logo"
+            src={logo}
+            alt="logo"
+            width="100px"
+            height="100px"
+          />
           <h1 className="navbar-brand">Space Traveler&apos;s Hub</h1>
           <div className="collapse navbar-collapse" />
           <ul className="nav justify-content-end">
             <li className="nave-item">
-              <NavLink className="nav-link" to="/">Rockets</NavLink>
+              <NavLink to="/">Rockets</NavLink>
             </li>
-            <li>
-              <NavLink className="nav-link" to="/missions">Missions</NavLink>
+            <li className="nave-item">
+              <NavLink to="/missions">Missions</NavLink>
             </li>
-            <li>
-              <NavLink className="nav-link" to="/profile">My Profile</NavLink>
+            <li className="nave-item">
+              <NavLink to="/profile">My Profile</NavLink>
             </li>
           </ul>
         </nav>
         <Switch>
-          <Route exact path="/">
-            <Rockets />
-          </Route>
           <Route exact path="/missions">
             <Missions />
           </Route>
           <Route exact path="/profile">
             <MyProfile />
+          </Route>
+          <Route exact path="/">
+            <Rockets />
           </Route>
         </Switch>
       </div>
--- src/components/missions/Missions.js
+++ src/components/missions/Missions.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const Missions = () => <div>Missions</div>;
 
 export default Missions;

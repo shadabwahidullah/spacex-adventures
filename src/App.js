4a52b9d87d9dd674af67c26b77bb353cea6ffdbf
shadabwahidullah <shadabwahidullah@gmail.com>
2021-11-17 02:05:04 +0430

    Add collapse style to my toggle menu button

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Missions from './components/Missions';
 import logo from './assets/img/logo.png';
 import MyProfile from './components/MyProfile';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/js/src/collapse';
 
 function App() {
   return (
@@ -19,13 +20,15 @@ function App() {
       <div className="container">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <div className="container">
-            <img className="logo" src={logo} alt="logo" width="100px" height="100px" />
+            <NavLink to="/rockets">
+              <img className="logo" src={logo} alt="logo" />
+            </NavLink>
             <h1 className="navbar-brand">Space Traveler&apos;s Hub</h1>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon" />
             </button>
             <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-              <ul className="nav ">
+              <ul className="navbar-nav">
                 <li className="nav-item">
                   <NavLink className="nav-link" activeClassName="active" to="/rockets">Rockets</NavLink>
                 </li>

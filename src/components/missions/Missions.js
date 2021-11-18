7da1cc6b460fd2158bc0747b67ab28c7ba3b5c6e
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-11-19 03:15:13 +0500

    Dispatch actions

--- src/components/missions/Missions.js
+++ src/components/missions/Missions.js
@@ -1,35 +1,43 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchMissions } from '../../redux/missions/missions';
 import './Missions.css';
+import {
+  fetchMissions,
+  joinMission,
+  leaveMission,
+} from '../../redux/missions/missions';
 
+let flag = true;
 const Missions = () => {
   const dispatch = useDispatch();
   const missions = useSelector((state) => state.missionsReducer);
 
   useEffect(() => {
-    dispatch(fetchMissions());
+    if (flag) {
+      dispatch(fetchMissions());
+      flag = !flag;
+    }
   }, []);
 
-  const [memberValue, setMemberValue] = useState('Not a Member');
-  const [memberClass, setMemberClass] = useState('not-member');
-  const [missionValue, setMissionValue] = useState('Join Mission');
-  const [missionClass, setMissionClass] = useState('join-mission');
+  // const [memberValue, setMemberValue] = useState('Not a Member');
+  // const [memberClass, setMemberClass] = useState('not-member');
+  // const [missionValue, setMissionValue] = useState('Join Mission');
+  // const [missionClass, setMissionClass] = useState('join-mission');
 
-  const handleMission = () => {
-    setMemberValue(
-      memberValue === 'Not a Member' ? 'Active Member' : 'Not a Member',
-    );
-    setMemberClass(
-      memberClass === 'not-member' ? 'active-member' : 'not-member',
-    );
-    setMissionValue(
-      missionValue === 'Join Mission' ? 'Leave Mission' : 'Join Mission',
-    );
-    setMissionClass(
-      missionClass === 'join-mission' ? 'leave-mission' : 'join-mission',
-    );
-  };
+  // const handleMission = () => {
+  //   setMemberValue(
+  //     memberValue === 'Not a Member' ? 'Active Member' : 'Not a Member'
+  //   );
+  //   setMemberClass(
+  //     memberClass === 'not-member' ? 'active-member' : 'not-member'
+  //   );
+  //   setMissionValue(
+  //     missionValue === 'Join Mission' ? 'Leave Mission' : 'Join Mission'
+  //   );
+  //   setMissionClass(
+  //     missionClass === 'join-mission' ? 'leave-mission' : 'join-mission'
+  //   );
+  // };
 
   return (
     <section>
@@ -43,24 +51,45 @@ const Missions = () => {
           </tr>
         </thead>
         <tbody>
-          {missions.map((mission) => (
+          {missions.missions.map((mission) => (
             <tr key={mission.mission_id}>
               <td>{mission.mission_name}</td>
               <td className="desc">{mission.description}</td>
-              <td>
-                <button type="button" className={`${memberClass}`}>
-                  {memberValue}
-                </button>
-              </td>
-              <td>
-                <button
-                  type="button"
-                  className={`btn ${missionClass}`}
-                  onClick={handleMission}
-                >
-                  {missionValue}
-                </button>
-              </td>
+              {mission.reserved ? (
+                <>
+                  <td>
+                    <button type="button" className="btn active-member">
+                      Active Member
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      type="button"
+                      className="btn leave-mission"
+                      onClick={() => dispatch(leaveMission(mission.mission_id))}
+                    >
+                      Leave Mission
+                    </button>
+                  </td>
+                </>
+              ) : (
+                <>
+                  <td>
+                    <button type="button" className="btn not-member">
+                      Not a Member
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      type="button"
+                      className="btn join-mission"
+                      onClick={() => dispatch(joinMission(mission.mission_id))}
+                    >
+                      Join Mission
+                    </button>
+                  </td>
+                </>
+              )}
             </tr>
           ))}
         </tbody>

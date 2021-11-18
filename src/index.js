3ae0136c60e067262bbd3cfdc32f086b1274ffc7
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-11-18 20:30:36 +0500

    Fix bugs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
-import store from './redux/store';
 import './index.css';
-import { Provider } from 'react-redux';
 import App from './App';
 import configureStore from './redux/store';
 
@@ -12,8 +10,6 @@ ReactDOM.render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
-    ,
-    {' '}
   </Provider>,
   document.getElementById('root'),
 );

84bff7da298735f27b25d93f4a73ca85053f214f
shadabwahidullah <shadabwahidullah@gmail.com>
2021-11-19 01:01:33 +0430

    Structure my profile page

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
-const MyProfile = () => <div>My Profile</div>;
+const MyProfile = () => (
+  <div className="container row">
+    <div className="col-md-6 pt-2">
+      <h2>My Missions</h2>
+    </div>
+    <div className="col-md-6 pt-2">
+      <h2>My Rockets</h2>
+    </div>
+  </div>
+);
 
 export default MyProfile;

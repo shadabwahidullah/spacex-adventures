d18de9acc1ddfeba5461e2390a85a40683526826
shadabwahidullah <shadabwahidullah@gmail.com>
2021-11-19 00:25:50 +0430

    Prevent fetching rockets from being called multiple times

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -3,11 +3,16 @@ import { useDispatch, useSelector } from 'react-redux';
 import Rocket from './Rocket';
 import { fetchRocketsCreator } from '../redux/RocketsReducer';
 
+let flag = true;
+
 const Rockets = () => {
   const rockets = useSelector((state) => state.rocketReducer);
   const dispatch = useDispatch();
   useEffect(() => {
-    fetchRocketsCreator(dispatch);
+    if (flag) {
+      fetchRocketsCreator(dispatch);
+      flag = !flag;
+    }
   }, []);
   return (
     <div className="rockets-list">

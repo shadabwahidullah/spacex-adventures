5c66ff6ed09bf44230702f57879c3d3132237265
shadabwahidullah <shadabwahidullah@gmail.com>
2021-11-19 22:31:24 +0430

    Add snapshot tests for Missions and My profile

--- src/__tests__/componets.react.test.js
+++ src/__tests__/componets.react.test.js
@@ -3,12 +3,34 @@ import Rockets from "../components/rockets/Rockets";
 import renderer from "react-test-renderer";
 import { Provider } from "react-redux";
 import store from "../redux/store";
+import Missions from "../components/missions/Missions";
+import MyProfile from "../components/MyProfile";
 
 describe("Component tests", () => {
-  test("Rockets test", () => {
+  test("Rockets snapshot test", () => {
     const rockets = renderer.create(
       <Provider store={store}>
         <Rockets />
+      </Provider>
+    );
+
+    let tree = rockets.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  test("Missions snapshot test", () => {
+    const rockets = renderer.create(
+      <Provider store={store}>
+        <Missions />
+      </Provider>
+    );
+
+    let tree = rockets.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  test("My profile snapshot test", () => {
+    const rockets = renderer.create(
+      <Provider store={store}>
+        <MyProfile />
       </Provider>
     );
 
